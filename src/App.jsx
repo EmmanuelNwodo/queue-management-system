7c15b64b89350f7a199eb49d32383637fd2e8f9e
Emmanuel Nwodo <nwodoemmanuel@gmail.com>
2026-04-09 21:53:41 +0100

    initializing the repository

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Queue Management System</h1>
+        <p>Manage your customer queues efficiently</p>ss
+      </header>
+    </div>
+  );
+}
